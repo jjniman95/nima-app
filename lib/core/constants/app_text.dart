f152c690725d6bb6ea0e41506060dab88b445a18
jjniman95 <j.j.nimantha@gmail.com>
2026-06-27 10:00:33 +0530

    Delete lib/core/constants/app_text.dart

--- lib/core/constants/app_text.dart
+++
@@ -1,6 +0,0 @@
-class AppText {
-  static const appName = 'NIMA';
-  static const meaning = 'Nearby Instant Meet App';
-  static const tagline = 'Meet safely, connect silently.';
-  static const slogan = 'Connecting people with consent.';
-}
