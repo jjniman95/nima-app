6098d0a73ea98b02c6c5633c8fb6cea60fd8ffc2
jjniman95 <j.j.nimantha@gmail.com>
2026-06-27 06:08:42 +0530

    Create these files

---
+++ lib/core/constants/app_text.dart
@@ -0,0 +1,6 @@
+class AppText {
+  static const appName = 'NIMA';
+  static const meaning = 'Nearby Instant Meet App';
+  static const tagline = 'Meet safely, connect silently.';
+  static const slogan = 'Connecting people with consent.';
+}
