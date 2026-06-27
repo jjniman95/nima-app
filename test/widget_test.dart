3975f5647b606ee5353d895f2f25fb7f95d4e67a
jjniman95 <j.j.nimantha@gmail.com>
2026-06-27 14:49:11 +0530

    Delete test directory

--- test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:nima_app/main.dart';
-
-void main() {
-  testWidgets('NIMA app loads', (tester) async {
-    await tester.pumpWidget(const NimaApp());
-    expect(find.text('NIMA'), findsOneWidget);
-  });
-}
