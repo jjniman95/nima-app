25b53cb2ecf86063f05d158ee642dc2a0a481d74
jjniman95 <j.j.nimantha@gmail.com>
2026-06-27 14:50:18 +0530

    Delete widget_test.dart

--- widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:nima_app/main.dart';
-
-void main() {
-  testWidgets('NIMA app starts', (tester) async {
-    await tester.pumpWidget(const NimaApp());
-    expect(find.text('NIMA'), findsWidgets);
-  });
-}
