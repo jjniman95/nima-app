266c29876be826886f7ee60674540ed6242018d3
jjniman95 <j.j.nimantha@gmail.com>
2026-06-27 14:53:12 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'core/theme/app_theme.dart';
+import 'features/splash/splash_screen.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const NimaApp());
+}
+
+class NimaApp extends StatelessWidget {
+  const NimaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'NIMA',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.system,
+      themeAnimationDuration: Duration.zero,
+      home: const SplashScreen(),
+    );
+  }
+}
