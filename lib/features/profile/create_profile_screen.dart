0fe2bd5b7b9053e3438402245765da18d49e92ad
jjniman95 <j.j.nimantha@gmail.com>
2026-07-03 14:47:50 +0530

    Update create_profile_screen.dart

--- lib/features/profile/create_profile_screen.dart
+++ lib/features/profile/create_profile_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../core/constants/app_colors.dart';
 import '../home/home_screen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class CreateProfileScreen extends StatefulWidget {
