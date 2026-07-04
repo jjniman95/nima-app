44a8e2bcf97ea3ff2cd54f8305b046ad618c50d6
jjniman95 <j.j.nimantha@gmail.com>
2026-07-04 10:13:57 +0530

    Create merges_screen.dart

---
+++ lib/features/merges/merges_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class ChatsScreen extends StatelessWidget {
+  const ChatsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      appBar: _ChatAppBar(),
+      body: Center(
+        child: Text('Unlimited chat will be added after Hi Requests.'),
+      ),
+    );
+  }
+}
+
+class _ChatAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const _ChatAppBar();
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(title: const Text('Chats'));
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
