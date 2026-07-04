0347c7bcb616435e2b1ee421c0c2471a1d4ba985
jjniman95 <j.j.nimantha@gmail.com>
2026-07-04 06:11:25 +0530

    Create merge_service.dart

---
+++ lib/services/merge_service.dart
@@ -0,0 +1,57 @@
+import 'dart:async';
+
+class MergeService {
+  MergeService._();
+
+  static final MergeService instance = MergeService._();
+
+  static const Duration mergeDuration = Duration(minutes: 10);
+
+  Timer? _timer;
+
+  Duration _remaining = mergeDuration;
+
+  final StreamController<Duration> _countdownController =
+      StreamController.broadcast();
+
+  Stream<Duration> get countdownStream => _countdownController.stream;
+
+  Duration get remaining => _remaining;
+
+  void start({
+    required VoidCallback onExpired,
+  }) {
+    _timer?.cancel();
+
+    _remaining = mergeDuration;
+    _countdownController.add(_remaining);
+
+    _timer = Timer.periodic(
+      const Duration(seconds: 1),
+      (timer) {
+        _remaining -= const Duration(seconds: 1);
+
+        _countdownController.add(_remaining);
+
+        if (_remaining.inSeconds <= 0) {
+          timer.cancel();
+          onExpired();
+        }
+      },
+    );
+  }
+
+  void reset() {
+    _remaining = mergeDuration;
+    _countdownController.add(_remaining);
+  }
+
+  void stop() {
+    _timer?.cancel();
+  }
+
+  void dispose() {
+    _timer?.cancel();
+    _countdownController.close();
+  }
+}
