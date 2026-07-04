f2dd64e6262b0c9f6a1619ad9dd628bf5602d2b0
jjniman95 <j.j.nimantha@gmail.com>
2026-07-04 10:30:01 +0530

    Update merges_screen.dart

--- lib/features/merges/merges_screen.dart
+++ lib/features/merges/merges_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class ChatsScreen extends StatelessWidget {
-  const ChatsScreen({super.key});
+class MergesScreen extends StatefulWidget {
+  const MergesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
